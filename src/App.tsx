2ff801864f763f1706350f69921f51504a4f4f24
Salida Maharjan <salidamaharjan@outlook.com>
2024-10-02 18:15:42 -0500

    Update CSS

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,12 @@ import Header from "./components/Header"
 
 function App() {
   return (
-      <>
+      <div className="bg-gray-100">
           <Header/>
           <h1 className="text-3xl font-bold text-green-600 underline">
               Hello world!
           </h1>
-      </>
+      </div>
 
   )
 }
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,7 @@
 import NavItem from "./NavItem.tsx";
+
 export default function Header() {
-    return <header className="flex flex-col md:flex-row shadow justify-between  items-center  p-2">
+    return <header className="flex bg-white flex-col md:flex-row shadow justify-between  items-center  p-2">
         <h1 className="text-3xl font-bold">Salida Maharjan</h1>
         <nav>
             <ul className="flex gap-4">

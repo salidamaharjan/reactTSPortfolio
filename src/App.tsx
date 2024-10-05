c8be3d2394a995d2278194dbc8eeddb5a877bdd3
Salida Maharjan <salidamaharjan@outlook.com>
2024-10-04 22:12:01 -0500

    My Journey and achievement section

--- src/App.tsx
+++ src/App.tsx
@@ -7,18 +7,29 @@ function App() {
   return (
       <div className="bg-gray-100 flex flex-col gap-1">
           <Header/>
-          <Section>
+          <Section className="flex justify-between gap-6 items-center">
               <div className="flex h-20 w-20 border items-center justify-center border-black rounded-full">
                   <Image src="" alt="Image"/>
               </div>
               <div className="flex flex-col gap-6 flex-1 p-4">
-                  <div className="font-bold text-xl">About Me</div>
-                  <div>Hello I am compassionate web developer.</div>
+                  <h2 className="font-bold text-xl">About Me</h2>
+                  <p>Hello I am compassionate web developer.</p>
                   <div className="flex gap-2">
                       <Button className="bg-blue-300">Biography</Button>
                       <Button>Experience</Button>
                   </div>
               </div>
+          </Section>
+          <Section className="flex-col">
+                <h3 className="font-bold text-lg">My Journey</h3>
+                <p>I started my coding journey in 2024.</p>
+                <h3 className="font-bold text-lg">Achievement</h3>
+                 <ul>
+                     <li>1</li>
+                     <li>2</li>
+                     <li>3</li>
+                     <li>4</li>
+                 </ul>
           </Section>
       </div>
 
--- src/components/Section.tsx
+++ src/components/Section.tsx
@@ -2,9 +2,10 @@ import {ReactNode} from "react";
 
 type SectionProps = {
     children: ReactNode,
+    className?: string
 }
-export default function Section({children}: SectionProps){
-    return <section className="flex justify-between md:w-[800px] md:self-center shadow bg-white gap-6 rounded-md mx-8 items-center pl-6">
+export default function Section({children, className}: SectionProps){
+    return <section className={`${className} md:w-[800px] md:self-center shadow bg-white rounded-md mx-8 pl-6`}>
         {children}
     </section>
 }
